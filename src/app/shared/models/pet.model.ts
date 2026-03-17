20fc31f5de96d366f3ad49ae64b015f803a6d9a1
Satria Pinandita Abyatarsyah <satriapinanditaabyatarsyah@gmail.com>
2026-03-18 00:48:42 +0700

    feat: add constants, models, and enums

--- src/app/shared/models/pet.model.ts
+++ src/app/shared/models/pet.model.ts
@@ -1,9 +1,11 @@
 import { Category } from './category.model';
+import { Tag } from './tag.model';
 
 export interface Pet {
   id?: number;
   name: string;
   category?: Category;
+  tags?: Tag[];
   photoUrls: string[];
   status?: 'available' | 'pending' | 'sold';
 }
