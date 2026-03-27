275bb2d3848790a10ec34032a2d6727c73fe3186
Satria Pinandita Abyatarsyah <satriapinanditaabyatarsyah@gmail.com>
2026-03-27 15:01:56 +0700

    fix: stop ignoring API errors and cleanup token if login fails

--- src/app/core/services/api/base-api.service.ts
+++ src/app/core/services/api/base-api.service.ts
@@ -1,23 +1,24 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 export abstract class BaseApiService {
   protected readonly http = inject(HttpClient);
 
   protected get<T>(url: string, params?: HttpParams | Record<string, string>): Observable<T> {
-    return this.http.get<T>(url, { params });
+    return this.http.get<T>(url, { params }).pipe(catchError((error) => throwError(() => error)));
   }
 
   protected post<T>(url: string, body: unknown): Observable<T> {
-    return this.http.post<T>(url, body);
+    return this.http.post<T>(url, body).pipe(catchError((error) => throwError(() => error)));
   }
 
   protected put<T>(url: string, body: unknown): Observable<T> {
-    return this.http.put<T>(url, body);
+    return this.http.put<T>(url, body).pipe(catchError((error) => throwError(() => error)));
   }
 
   protected delete<T>(url: string): Observable<T> {
-    return this.http.delete<T>(url);
+    return this.http.delete<T>(url).pipe(catchError((error) => throwError(() => error)));
   }
 }
